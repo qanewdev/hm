8dc01562593e71e03cce78fdfccaa2bf440af279
QAnewdev <qanewdev@gmail.com>
2018-01-31 21:44:42 +0200

    ver1

--- src/test/java/com/mob/webriver/Korzina.java
+++ src/test/java/com/mob/webriver/Korzina.java
@@ -5,7 +5,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+
 public class Korzina {
+
     @Test
     public void buy() {
         WebDriver driver = new FirefoxDriver();
