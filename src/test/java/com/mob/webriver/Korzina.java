b1dbf235a00f8be44420c42e628f050ac1daf96e
QAnewdev <qanewdev@gmail.com>
2018-02-06 21:06:27 +0200

    ver2

--- src/test/java/com/mob/webriver/Korzina.java
+++ src/test/java/com/mob/webriver/Korzina.java
@@ -7,16 +7,28 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 public class Korzina {
-
+<---Это скприпт проверкии
     @Test
-    public void buy() {
+    public void buy() throws InterruptedException {
         WebDriver driver = new FirefoxDriver();
         driver.navigate().to("http://hm-decor.com.ua");
         driver.findElement(By.cssSelector("h3.name")).click();
         driver.findElement(By.id("button-cart")).click();
         driver.findElement(By.id("cart-total")).click();
         driver.findElement(By.linkText("Оформление заказа")).click();
+        Thread.sleep(5000);
         driver.close();
     }
+Конец проверки
 
 }
+Начала дддддддд
+
+
+
+
+        аоаоававыа
+
+
+
+аваыа
